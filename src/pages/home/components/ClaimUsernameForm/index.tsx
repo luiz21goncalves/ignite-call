e99a5752b293e0d9b4992e877d7fd5635c982c2e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-12-24 08:52:53 -0300

    feat: add validation in claim username form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,17 +1,59 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import * as S from './styles'
 
-export function ClaimUsenameForm() {
-  return (
-    <S.Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="Seu usuário" />
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z0-9\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras, numeros e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
 
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </S.Form>
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsenameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  const formAnnotationText = errors.username
+    ? errors.username.message
+    : 'Digite o nome de usuário desejado'
+
+  return (
+    <>
+      <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </S.Form>
+
+      <S.FormAnnotation>
+        <Text>{formAnnotationText}</Text>
+      </S.FormAnnotation>
+    </>
   )
 }
