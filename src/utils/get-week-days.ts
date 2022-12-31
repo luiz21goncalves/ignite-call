cbb4bd513ab31ab008d8dc4a9e76dd0e5bceb3ae
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-12-31 09:01:23 -0300

    chore: add helper function to get weekdays

---
+++ src/utils/get-week-days.ts
@@ -0,0 +1,21 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+  const weekDaysIterator = Array(7).keys()
+
+  const weekDaysMap = Array.from(weekDaysIterator).reduce(
+    (weekDaysObject, day) => {
+      const weekDay = formatter.format(new Date(Date.UTC(2021, 5, day)))
+      const capitalizedWeekDay = weekDay
+        .substring(0, 1)
+        .toUpperCase()
+        .concat(weekDay.substring(1))
+
+      weekDaysObject[day] = capitalizedWeekDay
+
+      return weekDaysObject
+    },
+    {} as Record<number, string>,
+  )
+
+  return weekDaysMap
+}
