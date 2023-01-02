55f06df1282d4e4e846344e6c16efe60390f530a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-02 12:57:47 -0300

    chore: add coverage reporter to tests

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,12 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx}',
+    '!./src/**/*.stories.{tsx,mdx}',
+    '!./src/**/*.d.ts',
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
